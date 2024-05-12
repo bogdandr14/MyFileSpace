2b9966949365683ca58b169b9d0fd0879ef0770f
Bogdan Draghici <bogdandraghici99@gmail.com>
2024-05-12 16:02:21 +0300

    removed tagname alternate key

---
+++ MyFileSpace.Infrastructure.Migrations/Migrations/20240512130104_RemoveTagNameAlternateKey.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyFileSpace.Infrastructure.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveTagNameAlternateKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_User_TagName",
+                table: "User");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_User_TagName",
+                table: "User",
+                column: "TagName");
+        }
+    }
+}
