c1d1b6948d635003fff5c1e5d244dc4a1430681c
Bogdan Draghici <bogdandraghici99@gmail.com>
2024-05-20 23:25:45 +0300

    fix confirmation mail and tagname min length

--- MyFileSpace.Api/Program.cs
+++ MyFileSpace.Api/Program.cs
@@ -8,22 +8,16 @@ using Serilog;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.ConfigureKeyVault();
-
 // Add services to the container.
 builder.Host.UseSerilog((_, config) => config.ReadFrom.Configuration(builder.Configuration));
-
 builder.Services.AddModulesConfiguration(builder.Environment, builder.Configuration);
-
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
 app.Services.Initialize();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -38,19 +32,12 @@ else
     app.UseExceptionHandler(Constants.ERROR_PATH);
     app.UseHsts();
 }
-
 app.UserCorsConfiguration();
-
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseMiddleware<CustomExceptionHandlerMiddleware>();
-
 app.UseAuthentication();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 // Migrate and seed Database
