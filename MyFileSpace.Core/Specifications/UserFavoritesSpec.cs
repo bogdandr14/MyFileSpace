77b1da0d98df617cb15da5af63167b57c8327892
Bogdan Draghici <bogdandraghici99@gmail.com>
2024-05-24 23:43:59 +0300

    added get all favorites endpoint

---
+++ MyFileSpace.Core/Specifications/UserFavoritesSpec.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.Specification;
+using MyFileSpace.Infrastructure.Entities;
+
+namespace MyFileSpace.Core.Specifications
+{
+    internal class UserFavoritesSpec : Specification<StoredFile>, ISpecification<StoredFile>
+    {
+        public UserFavoritesSpec(Guid userId)
+        {
+            Query.Where(sf => sf.UsersFavorite.Any(uf => uf.UserId.Equals(userId)))
+                .Include(sf => sf.UsersFavorite)
+                .Include(sf => sf.AllowedUsers);
+        }
+    }
+}
